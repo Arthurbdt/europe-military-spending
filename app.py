19954852423ee469d3b86c7d6ae76a7556e1aa8a
Arthurbdt <boudet.arthur@gmail.com>
2022-05-16 21:34:30 +0200

    Modified app layout

--- app.py
+++ app.py
@@ -11,21 +11,54 @@ import pandas as pd
 data = pd.read_csv('europe_spending.csv')
 # slider data
 labels = data.columns.values[2:]
+fig2 = px.line(data_frame = data, x = 'Country', y= '2021')
 marks = {i: str(labels[i]) for i in range(0, len(labels), 2)}
 
 # ---------- Create dashboard components ----------
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
-title = dcc.Markdown(children = '# Military spending of European countries post Cold War')
-subtitle = dcc.Markdown(children = 'Expressed as percentage of country GDP')
-graph = dcc.Graph(figure={})
-slider = dcc.Slider(0, 30, step = None, marks = marks, value = 30)
+title = dcc.Markdown(
+    children = 
+        '''  
+        ### European military spending
+
+        [Source: SIPRI Military Expenditure Database](https://www.sipri.org/databases/milex)
+        '''
+        )
+subtitle = dcc.Markdown(children = 'Post Cold War, expressed as percentage of country GDP')
+map_europe = dcc.Graph(figure={})
+slider = dcc.Slider(min = 0, max = 30, step = None, marks = marks, 
+    value = 30, vertical = True, verticalHeight = 600)
+line_chart = dcc.Graph(figure = fig2)
 
 # ---------- Create dashboard layout ----------
-app.layout = dbc.Container([title, subtitle, graph, slider])
+'''
+format for layout
+dbc.Container()
+dbc.Row()
+dbc.Col()
+'''
+app.layout = dbc.Container([
+    dbc.Row([  
+        dbc.Col([title], width = 5)
+    ], justify = 'left'),
+
+    dbc.Row([
+        dbc.Col([''], width = 3)
+    ], justify = 'left'),
+
+    # graph and slider row
+    dbc.Row([
+        dbc.Col([slider], width = 1),
+        dbc.Col([map_europe], width = 11)
+    ], justify = 'center'),
+    dbc.Row([
+        dbc.Col([line_chart])
+    ])
+])
 
 # ---------- Create application callback ----------
 @app.callback(
-    Output(graph, 'figure'),
+    Output(map_europe, 'figure'),
     Input(slider, 'value')
 )
 def update_graph(column_name):
@@ -36,9 +69,11 @@ def update_graph(column_name):
                         locations = 'Country',
                         locationmode = "country names",
                         scope = "europe",
-                        height=600,
-                        color=marks[column_name],
+                        height =600,
+                        width = 1100,
+                        color = marks[column_name],
                         color_continuous_scale='ylgnbu')
+    fig.update_layout(margin=dict(t=10, b=0, l=0, r=0))
     return fig
 
 # Run app
