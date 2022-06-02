621abfd8f7722e61a942a064940c00e5260d8798
Arthurbdt <boudet.arthur@gmail.com>
2022-06-02 20:13:01 +0200

    Updated time series layout and explanation paragraph

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 '''
-Simple first app where the data from 2022 is displayed on a map
+Author: @Arthurbdt
+Web-app visualizing european militray spending since the end of the Cold War.
+App built with Dash, data from SIPRI
 '''
 
 from dash import Dash, dcc, Output, Input
@@ -11,8 +13,8 @@ import pandas as pd
 data = pd.read_csv('.\datasets\clean_data.csv')
 
 # slider data
-labels = data['Year'].unique()
-marks = {i: str(labels[i]) for i in range(0, len(labels), 2)}
+labels_year = data['Year'].unique()
+marks = {i: str(labels_year[i]) for i in range(0, len(labels_year), 2)}
 
 # create measures options for dropdown menu
 dropdown_options = [{'label': 'Percentage of GDP', 'value': 'Percent_gdp'},
@@ -28,10 +30,17 @@ title = dcc.Markdown(
 
         [Source: SIPRI Military Expenditure Database](https://www.sipri.org/databases/milex)
 
-        Here will be the explanation of all possible dropdown options:
-        - *Percentage of GDP*: tbd
-        - *US dollars (2021) per capita*: tbd
-        - *US dollars (2021):*: tbd
+        This web application visualizes the military spending of European countries since the end of the Cold War though
+        one of three variables:
+        - **Percentage of GDP:** percentage of gross domestic product allocated to military spending
+        - **US dollars (2021):** current dollars allocated to military spending (in $B)
+        - **US dollars (2021) per capita:** constant dollars per country inhabitant allocated to military spending 
+
+        Select the measure of interest in the dropdown menu to update all figures. You can visualize the evolution
+        of military spending over time by changing dates on the slider. Clicking on a country will filter out all
+        other countries on the time series chart.
+
+        You can select one or multiple countries on the time series chart using the legend on the left hand side.
         '''
         )
 
@@ -39,35 +48,31 @@ title = dcc.Markdown(
 map_europe = dcc.Graph(figure={})
 time_series = dcc.Graph(figure = {})
 
-# generate slider and dropdown menu
+# generate slider, dropdown
 year_slider = dcc.Slider(min = 0, max = 30, step = None, 
                     marks = marks, value = 30, 
-                    vertical = True, verticalHeight = 550)
+                    vertical = True, verticalHeight = 580)
 measure_dropdown = dcc.Dropdown(id = 'my-dpdn', multi = False, clearable = False, 
                                 value = 'Percent_gdp', options = dropdown_options)
 
 # ---------- Create dashboard layout ----------
 app.layout = dbc.Container([
     dbc.Row([  
-        dbc.Col([title], width = 5)
+        dbc.Col([title], width = 10)
     ], justify = 'left'),
 
     dbc.Row([  
         dbc.Col([measure_dropdown], width = 3)
-    ], justify = 'left'),
-
-    dbc.Row([
-        dbc.Col([''], width = 3)
-    ], justify = 'left'),
-
+    ], justify = 'left'),   
     # graph and slider row
     dbc.Row([
         dbc.Col([year_slider], width = 1),
         dbc.Col([map_europe], width = 11)
-    ], justify = 'center'),
+    ], justify = 'left', align = 'center'),
+    # radio items and time series
     dbc.Row([
-        dbc.Col([time_series])
-    ])
+        dbc.Col([time_series], width = 12)
+    ], align = 'center')
 ])
 
 # ---------- Create application callback ----------
@@ -86,36 +91,43 @@ def update_map(period, measure):
                         locations = 'Country',
                         locationmode = "country names",
                         scope = "europe",
-                        height =550,
+                        height = 580,
                         width = 1100,
                         color = measure,
                         color_continuous_scale='ylgnbu')
     fig.update_layout(margin=dict(t=10, b=0, l=0, r=0))
+    fig.update_geos(fitbounds="locations")
     return fig
 
 @app.callback(
     Output(time_series, 'figure'),      # update time series chart
     Input(map_europe, 'clickData'),     # country selected on map
-    Input(measure_dropdown, 'value')    # measure selected on dropdown menu  
+    Input(measure_dropdown, 'value')   # measure selected on dropdown menu
 )
 def update_ts(click, measure):
     if click:
-        # calculate something
+        # restrict dataset to selected country
         area = click['points'][0]['location']
         sample = data[data['Country']==area]
     else:
+        # plot all countries
         sample = data
     # update chart
-    fig = px.line(data_frame = sample, 
-                x = 'Year', 
-                y = measure,
-                color = 'Country',
-                height = 550)
-
+    fig = px.line(data_frame = sample, x = 'Year', y = measure, 
+                 color = 'Country', height = 500, template = 'simple_white')
+    fig.update_xaxes(showgrid=True)
+    fig.update_yaxes(showgrid=True)
+    # add NATO guidelines for percentage of gdp
+    if measure == 'Percent_gdp':
+        fig.add_shape(type = 'line', line_color = 'red', line_width = 3,
+            y0 = 2.0, y1 = 2.0, x0 = '1991', x1 = '2021', yref = 'y')
+        fig.add_shape(type = 'line', line_color = 'black', line_width = 0,
+            y0 = 0, y1 = 0, x0 = '1991', x1 = '2021', yref = 'y')
     # return graph
     return fig
 
 # Run app
 if __name__=='__main__':
     app.run_server(debug=True, port=8054)
+
 
